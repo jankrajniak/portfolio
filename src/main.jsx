25e6fdaff3ac7bccc362240ed495ea6ddf943e0a
jankrajniak <jan.krajniak@gmail.com>
2025-01-20 19:01:45 -0500

    main.jsx and App.jsx created, all pages created. router defined with all pages within main.jsx. Page structure with header, outlet and footer defined within App.jsx

---
+++ src/main.jsx
@@ -0,0 +1,46 @@
+// Installing the required packages
+import ReactDOM from 'react-dom/clien';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+// Import styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Bringing in the pages the router will use to conditionally show the appropraite views
+import App from './App';
+import AboutMePage from './pages/AboutMePage';
+import PortfolioPage from './pages/PortfolioPage';
+import ContactPage from './pages/ContactPage';
+import ResumePage from './pages/ResumePage';
+import ErrorPage from './pages/ErrorPage';
+
+// Define the accessible routes
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <AboutMePage />,
+            },
+            {
+                path: 'portfolio',
+                element: <PortfolioPage />,
+            },
+            {
+                path: 'contact',
+                element: <ContactPage />,
+            },
+            {
+                path: 'resume',
+                element: <ResumePage />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router = {router} />
+)
