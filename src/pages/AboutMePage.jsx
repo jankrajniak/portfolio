58d5bdfb3d93f24a219387c104d0c52d5e3c1669
jankrajniak <jan.krajniak@gmail.com>
2025-01-21 17:52:27 -0500

    ContactPage and corresponding form updated. Currently contains commented-out code for EmailJs, which will send an email to my email address with the submitted info. Need extra time to resolve issues with the package.Working on adding requirements for the form and updating the Footer next

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -3,7 +3,8 @@ import ProfilePicture from "../assets/images/Profile_Picture.jpg";
 export default function AboutMePage() {
     return (
         <div className="container bg-gradient p-5" style={{minHeight: "500px"}}>
-            <img src={ProfilePicture} className="rounded-circle w-25 h-auto img-fluid" alt="Profile Picture" />
+            <h2 className="mb-3">About Me</h2>
+            <img src={ProfilePicture} className="rounded-circle w-25 h-auto img-fluid mb-3" alt="Profile Picture" />
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus eius velit 
                 omnis ratione dignissimos saepe, illum necessitatibus eveniet nulla consectetur 
                 aliquam quasi esse alias porro in laudantium quos facilis dolores!
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,99 @@
+import { useState } from 'react';
+// import * as EmailJs from 'emailjs'
+// import dotenv from 'dotenv'
+// dotenv.config();
+
+
 export default function ContactPage() {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    const [successMessage, setSuccessMessage] = useState("");
+
+    const handleChange = (e) => {
+        setFormData({ ... formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form successfully submitted");
+        setSuccessMessage("Your message has been sent successfully. I will get back to you as soon as possible.");
+
+        // EmailJs.
+        //     send(
+        //         process.env.SERVICE_ID || '',
+        //         process.env.TEMPLATE_ID || '',
+        //         {
+        //             from_name: formData.name,
+        //             from_email: formData.email,
+        //             message: formData.message,
+        //         },
+        //         process.env.PUBLIC_KEY || ''
+        //     ).then (
+        //         (response) => {
+        //             console.log("SUCCESS", response.status, response.text);
+        //             setSuccessMessage("Your message has been sent successfully. I will get back to you as soon as possible.");
+        //             setFormData({
+        //                 name: "",
+        //                 email: "",
+        //                 message: "",
+        //             });
+        //         },
+        //         (error) => {
+        //             console.log("FAILED", error);
+        //             setSuccessMessage("Sorry, your message could not be sent. Please try again later.");
+        //         }
+        //     );
+    };
+
     return (
-        <div>
-            <h1>Contact Me</h1>
+        <div className = 'container'>
+            <h2>Contact Me</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className = "mb-3">
+                    <label htmlFor="message" className="form-label">Message</label>
+                    <textarea
+                        type="text"
+                        className="form-control"
+                        id="message"
+                        name="message"
+                        value={formData.message}
+                        onChange={handleChange}
+                        rowns="5"
+                        required
+                    ></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+            {<div className="alert alert-info mt-3">{successMessage}</div> ? successMessage : null}
         </div>
     )
 }
